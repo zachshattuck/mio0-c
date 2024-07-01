b33638541060ce0710ac6623a66ba97a16b99698
zachshattuck <zdogshattuck@gmail.com>
2024-07-01 16:07:31 -0600

    wip broken

--- main.c
+++ main.c
@@ -46,7 +46,59 @@ int is_valid_sig_bs(uint8_t* sig) {
   return 1;
 }
 
-void decompress_mio0_block(FILE* fp_rom, FILE* fp_out, int start) {
+
+void decompress_mio0_block(FILE* fp_rom, unsigned int fp_rom_size, FILE* fp_out, int start) {
+  int i = start;
+  fseek(fp_rom, start, 0L);
+
+
+  printf("%c", fgetc(fp_rom));
+  if(ferror(fp_rom)) {
+    fprintf(stderr, "Failure");
+  }
+  return;
+
+  if(i + 0x10 > fp_rom_size) {
+    fprintf(stderr, "Invalid MIO0 block");
+    return;
+  }
+
+  uint8_t header[0x10];
+  uint8_t c;
+  for(i = 0; i < 0x10; i++) {
+    c = fgetc(fp_rom);
+    header[i] = c;
+  }
+
+  uint32_t decompressed_length;
+  uint32_t compressed_offset;
+  uint32_t uncompressed_offset;
+
+  decompressed_length = (
+    (header[0x4] << 24)
+    & (header[0x5] << 16)
+    & (header[0x6] << 8)
+    & (header[0x7])
+  );
+
+  compressed_offset = (
+    (header[0x8] << 24)
+    & (header[0x9] << 16)
+    & (header[0xA] << 8)
+    & (header[0xB])
+  );
+
+  uncompressed_offset = (
+    (header[0xC] << 24)
+    & (header[0xD] << 16)
+    & (header[0xE] << 8)
+    & (header[0xF])
+  );
+
+  printf("Decompressed length: %d bytes\n", decompressed_length);
+  printf("Compressed offset: %d bytes\n", compressed_offset);
+  printf("Decompressed offset: %d bytes\n", uncompressed_offset);
+
 }
 
 int main(int argc, char** argv) {
@@ -78,7 +130,7 @@ int main(int argc, char** argv) {
     fprintf(stderr, "Failed to stat file: %s", FILENAME);
     return 1;
   }
-  int sz = st.st_size;
+  unsigned int sz = st.st_size;
   printf("File size: %d bytes\n", sz);
   if(sz < SIG_SIZE) {
     fprintf(stderr, "Invalid ROM file: Too small.\n");
@@ -122,7 +174,7 @@ int main(int argc, char** argv) {
   int count = 0;
   int locations[100];
   uint8_t c;
-  for(i = SIG_SIZE; i < sz; i++) {
+  for(i = SIG_SIZE; i < sz - 5; i++) {
     c = fgetc(fp_rom); // This could technically be EOF (-1)
     if(count == 100) {
       printf("Reached max MIO0 sections (because I'm lazy)\n");
@@ -144,14 +196,14 @@ int main(int argc, char** argv) {
     fseek(fp_rom, 0L, i);
   }
 
-  printf("Found %d instances of \"MIO0\"\n", count);
+  printf("Found %d instances of \"MIO0\"\n\n", count);
 
 
 
 
 
   for(i = 0; i < count; i++) {
-    printf("DECOMPRESSING BLOCK 1: 0x%x ----------------\n", locations[i]);
+    printf("DECOMPRESSING BLOCK %d: 0x%x ----------------\n", i, locations[i]);
 
     char filename[30];
     snprintf(filename, 30, "0x%x.texture", locations[i]);
@@ -162,9 +214,9 @@ int main(int argc, char** argv) {
       fprintf(stderr, "Failed to open file: %s", FILENAME);
       continue;
     }
-    printf("Opened file: %s\n", FILENAME);
+    printf("Opened file: %s\n", filename);
 
-    decompress_mio0_block(fp_rom, fp_outfile, locations[i]);
+    decompress_mio0_block(fp_rom, sz, fp_outfile, locations[i]);
 
     printf("\n");
     fclose(fp_outfile);
